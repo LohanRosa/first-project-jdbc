011aec2e8bfda212f6d517d8dd6115fd61350b8a
Lohan Rosa <Lrosa@cisbaf.local>
2025-05-13 12:22:46 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,6 +37,12 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
 		
+		
+		System.out.println("\n=== TEST 4: seller Update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	}
 
 }
